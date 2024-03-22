b68d923f45b035498ed586488245bdc1ba188c1e
Hazar Yasin Sezgin <sezgihaz@fit.cvut.cz>
2024-03-22 18:34:52 +0100

    Restructuring the code

--- app.js
+++ app.js
@@ -4,57 +4,67 @@ function minBitsForTwosComplement(num) {
     else if (num >= -2147483648) return 32;
     else return 64;
 }
+
 function bitPadding(array, num){
     let numBits = minBitsForTwosComplement(num);
     let sizeArray = array.length;
-            if(sizeArray % numBits != 0){
-                let toFill = numBits - (sizeArray % numBits);
-                for(let l = 0 ; l < toFill ; l++){
-                    array.unshift(0);
-                }
-            }
-    return array;
-}
-
-function bitNegation(array){
-    sizeArray = array.length
-            for(let i = 0 ; i < sizeArray ; i++){
-                if( array[i] == 0){
-                    array[i] = 1;
-                }
-                else array[i] = 0;
-            }
-    return array;        
-}
-
-function addOne(array){
-    for(let k = sizeArray-1 ; k > 0 ; k--){
-        if( array[k] == 0){
-            array[k] = 1;
-            //ukončím for cyklus
-            break;
-        }
-        else{
-            //to znamená že se array[index] == 1, tak musíme změnit hodnotu z 1 na 0 a v dalším cyklu se podívat na další bit
-            array[k] = 0;
-        }
+    let toFill = 0;
+    if(sizeArray % numBits != 0){
+        toFill = numBits - (sizeArray % numBits);
+    }
+    for(let l = 0 ; l < toFill; l++){ // Adjusted to remove the additional 8 bits
+        array.unshift(0);
     }
     return array;
 }
 
+function bitNegation(array){
+    let sizeArray = array.length; // Added missing 'let' declaration
+    for(let i = 0 ; i < sizeArray ; i++){
+        if( array[i] == 0){
+            array[i] = 1;
+        }
+        else array[i] = 0;
+    }
+    return array;        
+}
+
+function addOne(array){
+    let sizeArray = array.length; // Added missing 'let' declaration
+    for(let k = sizeArray-1 ; k >= 0 ; k--){ // Adjusted condition to include 0
+        if( array[k] == 0){
+            array[k] = 1;
+            return array;
+        }
+        else{
+            array[k] = 0;
+        }
+    }
+    array.unshift(1); // Handles the case when all bits are 1
+    return array;
+}
+
 function twosComplement(num, array){
+    array = bitPadding(array, num);
+    array = bitNegation(array);
+    array = addOne(array);
+    return array;
+}
+
+function getBits(num){
+    const convertBase = 2;
+    let array = [];
+    let tempNum = num;
+
+    while(tempNum > 0){
+        array.push(tempNum % convertBase);
+        tempNum = Math.floor(tempNum / convertBase);
+    }
     
-    array = bitPadding(array, num)        
-    
-    array = bitNegation(array)       
-            
-    array = addOne(array)      
-            
-    return array
+    return array.reverse();
 }
 
 function decimalToBinary() {
-    const convertBase = 2;
     let decimalInput = document.getElementById('decimalInput').value;
     let numericInput = Number(decimalInput);
     
@@ -65,25 +75,17 @@ function decimalToBinary() {
         document.getElementById('binaryResult').innerText = "Výsledek je: " + numericInput;
     }
     else{
-        let isNegative = false;
-        if(numericInput < 0){
-            console.log("Negative");
-            isNegative = true;
-            numericInput = Math.abs(numericInput)
-        }
-        let array = [];
-        let tempNum = numericInput;
-
-        while(tempNum > 0){
-            array.push(tempNum % convertBase);
-            tempNum = Math.floor(tempNum / convertBase);
-        }
-
-        let result = array.reverse();
+        let isNegative = numericInput < 0; // Adjusted to use boolean directly
         if(isNegative){
-            result = twosComplement(numericInput,result);
+            numericInput = Math.abs(numericInput);
         }
-    
-        document.getElementById('binaryResult').innerText = "Výsledek je: " + result.join("");
+        let result = getBits(numericInput);
+        if(isNegative){
+            result = twosComplement(numericInput, result);
+            document.getElementById('binaryResult').innerText = "Výsledek v dvojkovém doplňku je: " + result.join("");
+        }
+        else{
+            document.getElementById('binaryResult').innerText = "Výsledek v binární soustavě je: " + result.join("");
+        }
     }
 }
